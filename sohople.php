2a05eedb47affc11fcebc45095d23ff7f390efea
vovantuan2002 <votuant2@gmail.com>
2021-12-10 09:52:37 +0700

    sohpople1

---
+++ sohople.php
@@ -0,0 +1,14 @@
+
+<?php
+ function calculate($a,$b){
+    echo $a + $b;
+    echo "<br>";
+    echo $a - $b;
+    echo "<br>";
+    echo $a * $b;
+    echo "<br>";
+    echo $a / $b;
+ }
+ 
+    calculate(5,0);
+   
